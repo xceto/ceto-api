6082157fb070d506d83b04442f524cd9ecc8d325
Claudio Troncoso <ctroncoso@goplaceit.com>
2017-05-22 18:24:04 -0400

    Cambios varios

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,30 +1,41 @@
 class DashboardController < ApplicationController
 
 	def index
-		resultado = JSON.parse(open("https://sleepy-reaches-57394.herokuapp.com/api/v1?bandera=2").read)
-		@resultado_json = resultado
+		
+		busqueda = ""
+		bandera = "2"
+
+		if params['/dashboard'].present? && params['/dashboard']['estado'].present? && params['/dashboard']['desde'].present? && params['/dashboard']['hasta'].present?
+		 bandera = "1"
+		 busqueda = "&estado=#{params['/dashboard']['estado']}&desde=#{params['/dashboard']['desde']}&hasta=#{params['/dashboard']['hasta']}"	
+		end
+
+	@resultado_json = JSON.parse(open("https://sleepy-reaches-57394.herokuapp.com/api/v1?bandera=#{bandera}"+busqueda).read)
+
 	end
 
 	def new
-
 		revisa_cedidos = JSON.parse(open("https://sleepy-reaches-57394.herokuapp.com/api/v1?bandera=3").read)
-
 		revisa_cedidos.each do |verifica|
 		 	revisa_existencia = Dashboard.exists?(:folio => verifica['folio'])
 
 			if revisa_existencia == false
 				 Dashboard.create(folio: verifica['folio'], monto: verifica['monto'],rut_pagador: verifica['rut_pagador'],
 					rut_cedido: verifica['rut_cedido'], estado: verifica['estado'], fecha_creacion: verifica['created_at'], fecha_update: verifica['updated_at'])
+				flash[:notice] = "Guardados Correctamente"
 			else
-				puts "ya existe"
+				flash[:notice] = "No hay nuevos datos"
 			end
 	  end
-	  render :show
+	  redirect_to action: "index"
 	end
 
 	def show
 		@cesiones_cedidas = Dashboard.all.order(:created_at)
 	end
 
+	def buscador
+		byebug
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
+post 'dashboard' => 'dashboard#index', as: :buscador
+
 root to: "dashboard#index"
+
 resources :dashboard
 
 post 'dashboard/new', to: 'dashboard#new'
